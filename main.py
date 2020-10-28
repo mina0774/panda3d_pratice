0bcca753c6e1c889bc25b509001cc6efd54fe7c9
mina0774 <mina0774@hanmail.net>
2020-10-29 01:09:13 +0900

    load scenery, control camera

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from direct.showbase.ShowBase import ShowBase
-# panda3d window
-class MyGame(ShowBase):
-    def __init__(self):
-        super().__init__()
+from math import pi, sin, cos
 
-game=MyGame()
-game.run()
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+        # Add the spinCameraTask procedure to the task manager.
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+    # Define a procedure to move the camera.
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 30.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 10)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
+
+app = MyApp()
+app.run()
